b287aff4f22e068289fc721d5092aa59b73dbad7
suebin78 <ksb060708@hanyang.ac.kr>
2025-07-19 01:51:43 +0900

    create 기능

--- src/App.js
+++ src/App.js
@@ -34,12 +34,14 @@ function Header(props){
         }}>{props.title}</a></h1>
     </header>
 }
-function Create(){
+function Create(props){
     return <article>
         <h2>Create</h2>
         <form onSubmit={(event)=>{
             event.preventDefault();
             const title = event.target.title.value;
+            const body = event.target.body.value;
+            props.onCreate(title, body);
         }}>
             <p><input type="text" name="title" placeholder="title"/></p>
             <p><textarea name="body" placeholder="body"></textarea></p>
@@ -50,11 +52,12 @@ function Create(){
 function App() {
     const [mode, setMode] = useState('WELCOME');
     const [id, setId] = useState(null);
-    const topics = [
+    const [nextId, setNextId] = useState(4);
+    const [topics, setTopics] = useState([
         {id:1, title:'html', body:'html is ...'},
         {id:2, title:'css', body:'css is ...'},
         {id:3, title:'javascript', body:'javascript is ...'}
-    ]
+    ]);
     let content = null;
     if(mode==='WELCOME'){
         content = <Article title="Welcome" body="Hello, WEB"></Article>
@@ -69,8 +72,14 @@ function App() {
         content = <Article title={title} body={body}></Article>
     }
     else if(mode === 'CREATE'){
-        content = <Create onCreate={(title, body)=>{
-
+        content = <Create onCreate={(_title, _body)=>{
+            const newTopic = {id:nextId, title:_title, body:_body}
+            const newTopics = [...topics]
+            newTopics.push(newTopic);
+            setTopics(newTopics);
+            setMode('READ');
+            setId(nextId);
+            setNextId(nextId+1);
         }}></Create>
     }
   return (
